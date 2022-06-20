26521acd45f24f6a3c83d1cc1cbd03c6ff3d06b0
Cengizhan Atakul <cengizatakul@hotmail.de>
2022-06-20 16:54:35 +0200

    js navbar & heading

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,7 @@
 export function navigation() {
   const pages = document.querySelectorAll('.page');
-  const navLinks = document.querySelectorAll('[data-js=nav-link]');
-  const headline = document.querySelector('[data-js=headline]');
+  const navLinks = document.querySelectorAll('[data-js=navLink]');
+  const headline = document.querySelector('[data-js=headline]')
   navLinks.forEach(navLink => {
     navLink.addEventListener('click', () => {
       const selectedPage = document.querySelector(
@@ -15,3 +15,18 @@ export function navigation() {
     });
   });
 }
+// export function navigation() {
+//   const pages = document.querySelectorAll('.page')
+//   const navLinks = document.querySelectorAll('[data-js="navLink"]')
+//   navLinks.forEach(navLink => {
+//       navLink.addEventListener('click', () => {
+//         const selectedPage = document.querySelector(
+//           `[data-js=${navLink.dataset.link}]`
+//           );
+//         pages.forEach(page => {
+//           page.classList.remove('current');
+//         });
+//         selectedPage.classList.add('current');
+//       });
+//     });
+// }
